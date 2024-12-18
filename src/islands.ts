ff12ec7cbf48d87e9dfaffaa9d595cf51992b37c
horrible little slime <69secret69email69@gmail.com>
2024-12-18 17:00:25 -0500

    add spirit currencies

--- src/islands.ts
+++ src/islands.ts
@@ -1,11 +1,12 @@
-import { Element, Location, Monster } from "kolmafia"
-import { $element, $location, $monster } from "libram"
+import { Element, Item, Location, Monster } from "kolmafia"
+import { $element, $item, $location, $monster } from "libram"
 
 export type HolidayIsland = {
   location: Location,
   orbTarget: Monster,
   element: Element,
   choice: number,
+  currency: Item,
 }
 
 const Easter: HolidayIsland = {
@@ -14,7 +15,9 @@ const Easter: HolidayIsland = {
   element: $element`Stench`,
     // eslint-disable-next-line libram/verify-constants
   orbTarget: $monster`moai`,
-  choice: 1538
+  choice: 1538,
+    // eslint-disable-next-line libram/verify-constants
+  currency: $item`Spirit of Easter,`
 };
 
 const StPatrick: HolidayIsland = {
@@ -23,7 +26,9 @@ const StPatrick: HolidayIsland = {
     element: $element`Sleaze`,
   // eslint-disable-next-line libram/verify-constants
   orbTarget: $monster`giant potato`,
-  choice: 1539
+  choice: 1539,
+  // eslint-disable-next-line libram/verify-constants
+  currency: $item`Spirit of St. Patrick's Day`
 }
 
 export default { Easter, StPatrick }
