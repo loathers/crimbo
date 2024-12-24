8dda555498a3573c6bf16b6fa8dbb5883e5c6883
horrible little slime <69secret69email69@gmail.com>
2024-12-24 14:56:16 -0500

    equip gnomish housemaid's kgnee on islands

--- src/outfit.ts
+++ src/outfit.ts
@@ -60,7 +60,7 @@ function mergeSpecs(...outfits: OutfitSpec[]): OutfitSpec {
 }
 
 const adventuresFamiliars = (allowEquipment?: boolean) =>
-  allowEquipment ? $familiars`Temporal Riftlet, Reagnimated Gnome` : $familiars`Temporal Riftlet`;
+  allowEquipment && have($item`gnomish housemaid's kgnee`) ? $familiars`Temporal Riftlet, Reagnimated Gnome` : $familiars`Temporal Riftlet`;
 const chooseFamiliar = (options: MenuOptions = {}): Familiar => {
   if (options.location?.zone === "Holiday Islands") {
     if (args.shrub && get("shrubGifts") === "gifts") return $familiar`Crimbo Shrub`;
@@ -235,6 +235,8 @@ export function islandOutfit(
     allowEquipment: !usingOrb,
     allowAttackFamiliars: fight === "regular",
   });
+
+  if (outfit.familiar === $familiar`Reagnimated Gnome`) outfit.equip($item`gnomish housemaid's kgnee`)
 
   if (shouldPickpocket() && myPrimestat() !== $stat`Moxie`)
     outfit.equip(ifHave("acc2", $item`mime army infiltration glove`));
